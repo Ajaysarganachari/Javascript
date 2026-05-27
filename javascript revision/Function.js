0781c3d7b78610a4a6ac375795dbc66c2d15f00b
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2026-05-27 13:26:15 +0530

    Update Function.js

--- javascript revision/Function.js
+++ javascript revision/Function.js
@@ -25,3 +25,17 @@ let arr=[1,2,3,4,5,6,7,8,9,10];
 arr.forEach((arr)=>{
     console.log(arr*arr);
 });
+
+
+map() Method Definition
+
+The map() method in JavaScript is an array method used to create a new array by applying a function to each element of the original array
+
+
+let numbers = [1, 2, 3, 4];
+
+let doubled = numbers.map(function(num) {
+  return num * 2;
+});
+
+console.log(doubled);
